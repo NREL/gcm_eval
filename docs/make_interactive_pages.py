5e7abe9aaa8bc478dc8e1fad57f66a0d0dab5d51
grantbuster <grant.buster@nrel.gov>
2024-10-04 08:38:31 -0600

    added map clarification for utility spatial boundary

--- docs/make_interactive_pages.py
+++ docs/make_interactive_pages.py
@@ -69,7 +69,7 @@ if __name__ == '__main__':
                 f.write('Note that we used a simple state mask, and the '
                         'region may not perfectly match the spatial boundary '
                         'of the true NERC region.\n\n')
-            elif region in UTILITIES:
+            elif 'Utility Partner' in region:
                 f.write('Note that we used a simple state mask, and the '
                         'region will not perfectly match the '
                         'true Utility boundary.\n\n')
